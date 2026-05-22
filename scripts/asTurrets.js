ac2c3b53367fb1e60ab3c76be4e5dc35d35cd5b1
Sputnuc <asdasdasdboom123@gmail.com>
2026-05-22 23:21:39 +0500

    Quality of life changes

--- scripts/asTurrets.js
+++ scripts/asTurrets.js
@@ -1,4 +1,4 @@
-const splinter = extend(ItemTurret, "splinter", {});
+/* const splinter = extend(ItemTurret, "splinter", {});
 
 const decay = extend(ItemTurret, "decay", {});
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,20 @@
 require("asAttributes");
 require("asUnits");
 require("asBlocks");
-require("asTurrets");
+// require("asTurrets");
 require("asFactions")
 
-let yourlasttick = Vars.tree.loadMusic("yourLastTick");
+let yourLastTick = Vars.tree.loadMusic("yourLastTick");
 let ascendancy = Vars.tree.loadMusic("ascendancy");
-let fearintheair = Vars.tree.loadMusic("fearInTheAir");
+let fearInTheAir = Vars.tree.loadMusic("fearInTheAir");
 let importantTerritory = Vars.tree.loadMusic("importantTerritory");
 let oldBossArray = Vars.control.sound.bossMusic
 
 
 Events.on(ClientLoadEvent, e => {
-	Vars.control.sound.darkMusic.add(yourlasttick);
-	Vars.control.sound.ambientMusic.add(fearintheair);
-	Vars.control.sound.ambientMusic.add(importantTerritory);
+	Vars.control.sound.darkMusic.add(yourLastTick);
+	Vars.control.sound.darkMusic.add(importantTerritory);
+	Vars.control.sound.ambientMusic.add(fearInTheAir);
 	// Vars.control.sound.bossMusic.add(ascendancy); this will be on some important sectors.
 });
 
